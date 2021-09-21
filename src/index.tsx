9684fe3859b82da808011ea6aff9a9dddb6aad2b
Seimei Matsusaki <smatsusaki@gmail.com>
2021-09-21 17:55:16 -0400

    Installed css loaders and made it work

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import './style.css';
 
 const text: string = 'Hello React';
 const Test = <p>Greeting From React</p>;
 
-console.log(text);
 ReactDOM.render(Test, document.getElementById('root'))
