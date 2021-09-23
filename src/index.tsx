2dc77146309cd4129113566383affef1355ae039
Seimei Matsusaki <seimatsusaki@gmail.com>
2021-09-22 21:16:55 -0400

    Extract render

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import React from 'react';
 import BitCalc from './components';
 import './style.css';
 
-ReactDOM.render(<BitCalc />, document.getElementById('root'))
+render(<BitCalc />, document.getElementById('root'))
