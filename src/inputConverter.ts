580c6e207ad3844936569020aa3e4af75987af5d
Seimei Matsusaki <seimatsusaki@gmail.com>
2021-10-07 18:57:19 -0400

    Added implementaion to input converter

--- src/inputConverter.ts
+++ src/inputConverter.ts
@@ -1,7 +1,7 @@
 import { NumBase } from './enum';
 
 
-export default function convertUserInput (input: string, base: NumBase) {
+export default function convertUserInput (input: string, base: NumBase): number {
 
     if (typeof input !== 'string') 
         return 0;
@@ -13,15 +13,49 @@ export default function convertUserInput (input: string, base: NumBase) {
 
     if (input === '-')
         return 0; 
+
+    let result: number = 0;
+
+    switch (base) {
+        case NumBase.BIN:
+            result = convertBinIntoDec(input);
+            break;
+        case NumBase.DEC:
+            result = convertDecStrIntoDec(input);
+            break;
+        case NumBase.HEX:
+            result = convertHexIntoDec(input);
+            break;
+    }
     
-    
+    return result;
 }
 
-function isValidDec (input: string) {
-    const re = /^\d+$/;
+function convertDecStrIntoDec (input: string): number {
+    const upper_limit = 2147483647;
+    const lower_limit = -2147483648;
+    const re1 = /^-?(\d*\.)?\d+$/;
+    const re2 = /^-?\d+$/;
+    let result = 0;
+
+    if (re1.test(input))
+        throw new Error('No support for fractions');
+
+    if (!re2.test(input))
+        throw new Error('Invalid character(s) found');
+    
+    result = parseInt(input);
+
+    if (result > upper_limit)
+        throw new Error('Value is too large');
+
+    if (result < lower_limit)
+        throw new Error('value is too small');
+
+    return result;
 }
 
-function convertBinIntoDec (input: string) {
+function convertBinIntoDec (input: string): number {
     const re = /^[01]{0,32}+$/;
 
     if (input.length > 32) 
@@ -33,7 +67,7 @@ function convertBinIntoDec (input: string) {
     return parseInt(input, 2);
 }
 
-function convertHexIntoDec (input: string) {
+function convertHexIntoDec (input: string): number {
     const upper_limit = 0x7fffffff;
     const lower_limit = -0x80000000;
     const re = /^-?([A-F]|[0-9])+$/;
