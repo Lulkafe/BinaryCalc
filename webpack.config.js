c4a3c8914ffbe7f99f8397726a7ed2b852fb8894
Seimei Matsusaki <seimatsusaki@gmail.com>
2022-03-27 11:56:43 -0400

    Adjusted for Github Page deployment

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ const stylesHandler = 'style-loader';
 const config = {
     entry: './src/index.tsx',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
     },
     plugins: [
         new HtmlWebpackPlugin({
