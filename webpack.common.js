b1f4c4f98ef90d2b08a7547ee76c9bddafe53f2f
Seimei Matsusaki <seimatsusaki@gmail.com>
2022-04-04 17:18:17 -0400

    Updated favicon and webpack configrations

--- webpack.common.js
+++ webpack.common.js
@@ -1,28 +1,18 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const isProduction = process.env.NODE_ENV == 'production';
-
-
 const stylesHandler = 'style-loader';
 
-
-
-const config = {
+module.exports = {
     entry: './src/index.tsx',
     output: {
         path: path.resolve(__dirname, 'docs'),
+        clean: true
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
             favicon: './src/favicon.ico',
         }),
-
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
     ],
     module: {
         rules: [
@@ -39,9 +29,6 @@ const config = {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
                 type: 'asset',
             },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
         ],
     },
     resolve: {
@@ -49,13 +36,4 @@ const config = {
     },
 };
 
-module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
-        
-        
-    } else {
-        config.mode = 'development';
-    }
-    return config;
-};
+
